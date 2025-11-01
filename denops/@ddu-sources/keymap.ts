5beacd782362853016969804835f43ab8091d418
kamecha <50443168+kamecha@users.noreply.github.com>
2025-11-01 21:39:17 +0900

    source での表示を :map ライクに変更

--- denops/@ddu-sources/keymap.ts
+++ denops/@ddu-sources/keymap.ts
@@ -14,14 +14,32 @@ export class Source extends BaseSource<Params> {
     return new ReadableStream({
       async start(controller) {
         const items: Item<ActionData>[] = [];
+        // TODO: callback parse doesn't work yet
         const keymaps = await fn.maplist(args.denops) as ActionData[];
 
+        // keymap to display string like `:help map-listing` function
+        const mapListing = (
+          mode: string,
+          lhs: string,
+          attr: string,
+          rhs: string,
+        ): string => {
+          const padding = " ";
+          let line = "";
+          line += mode + " ".repeat(2 - mode.length) + padding;
+          line += lhs + (lhs.length <= 11 ? " ".repeat(11 - lhs.length) : "") + padding;
+          line += attr + " ".repeat(2 - attr.length);
+          line += rhs;
+          return line;
+        };
+
         for (const keymap of keymaps) {
+          // TODO: "&" の表示を調べとく
+          let attr = keymap.noremap ? "*" : keymap.script ? "&" : " ";
+          attr += keymap.buffer ? "@" : " ";
           items.push({
-            word: keymap.mode + ": " + keymap.lhs +
-              (keymap.noremap ? "\t-* " : "\t-> ") +
-              (keymap.rhs ? keymap.rhs : "<anonymous>") +
-              (keymap.desc ? "\t| " + keymap.desc : ""),
+            word: mapListing(keymap.mode, keymap.lhs, attr, keymap.rhs),
+            // TODO: SpecialKey でハイライトもしたい
             action: keymap,
           });
         }
