6e8259718f44538139b191818c1379c68149a792
kamecha <50443168+kamecha@users.noreply.github.com>
2025-11-01 10:10:14 +0900

    とりあえず動くとくまで作った

--- denops/@ddu-kinds/keymap.ts
+++ denops/@ddu-kinds/keymap.ts
@@ -1,13 +1,52 @@
+import { DduItem } from "jsr:@shougo/ddu-vim@11.1.0/types";
+import { ActionFlags, Actions } from "jsr:@shougo/ddu-vim@11.1.0/types";
 import { BaseKind } from "jsr:@shougo/ddu-vim@11.1.0/kind";
+import * as fn from "jsr:@denops/std@8.0.0/function";
+import type { Denops } from "jsr:@denops/std@8.0.0";
 
 type Params = Record<never, never>;
 
-export type ActionData = Record<never, never>;
+// :help mapping-dict
+export type ActionData = {
+  lhs: string;
+  lhsraw: Uint8Array;
+  lhsrawalt?: Uint8Array;
+  rhs: string;
+  silent: 0 | 1;
+  noremap: 0 | 1;
+  script: 0 | 1;
+  expr: 0 | 1;
+  buffer: 0 | 1;
+  mode: string;
+  sid: number;
+  scriptversion: number;
+  lnum: number;
+  nowait: 0 | 1;
+  abbr: 0 | 1;
+  mode_bits: number;
+
+  // neovim only? not documented in :help mapping-dict
+  desc?: string;
+  callback?: string;
+};
 
 export class Kind extends BaseKind<Params> {
-  override actions = {};
+  override actions: Actions<Params> = {
+    type: async (args: {
+      denops: Denops;
+      items: DduItem[];
+    }) => {
+      const action = args.items[0].action as ActionData;
+      if (action.mode !== "n") {
+        return ActionFlags.None;
+      }
+      // TODO: For special strings, it may not work correctly, so we want to escape them properly.
+      await fn.feedkeys(args.denops, action.lhs);
+      return ActionFlags.None;
+    },
+  };
 
   override params(): Params {
-    throw new Error("Method not implemented.");
+    return {};
   }
 }
--- denops/@ddu-sources/keymap.ts
+++ denops/@ddu-sources/keymap.ts
@@ -1,17 +1,38 @@
 import { Item } from "jsr:@shougo/ddu-vim@11.1.0/types";
-import { BaseSource } from "jsr:@shougo/ddu-vim@11.1.0/source";
+import { BaseSource, GatherArguments } from "jsr:@shougo/ddu-vim@11.1.0/source";
 import { ActionData } from "../@ddu-kinds/keymap.ts";
+import * as fn from "jsr:@denops/std@8.0.0/function";
 
 type Params = Record<never, never>;
 
 export class Source extends BaseSource<Params> {
   override kind = "keymap";
 
-  override gather(): ReadableStream<Item<ActionData>[]> {
-    throw new Error("Not implemented");
+  override gather(
+    args: GatherArguments<Params>,
+  ): ReadableStream<Item<ActionData>[]> {
+    return new ReadableStream({
+      async start(controller) {
+        const items: Item<ActionData>[] = [];
+        const keymaps = await fn.maplist(args.denops) as ActionData[];
+
+        for (const keymap of keymaps) {
+          items.push({
+            word: keymap.mode + ": " + keymap.lhs +
+              (keymap.noremap ? "\t-* " : "\t-> ") +
+              (keymap.rhs ? keymap.rhs : "<anonymous>") +
+              (keymap.desc ? "\t| " + keymap.desc : ""),
+            action: keymap,
+          });
+        }
+
+        controller.enqueue(items);
+        controller.close();
+      },
+    });
   }
 
   override params(): Params {
-    throw new Error("Not implemented");
+    return {};
   }
 }
